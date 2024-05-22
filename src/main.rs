85f8878d18b8d85e5e3a0c38234da326a9ab7d75
g610g <gio.gonzales@carsu.edu.ph>
2024-05-22 17:10:58 +0800

    concurrent tokio-ing

--- src/main.rs
+++ src/main.rs
@@ -17,17 +17,41 @@ async fn bar() -> String {
 fn new_sharded_db(shared_num: usize) -> shardedDb {
     Arc::new(DashMap::with_capacity(shared_num))
 }
-
+async fn one_second() {
+    sleep(Duration::from_millis(1000)).await;
+    println!("one second sleep is done");
+}
+async fn two_second() {
+    sleep(Duration::from_millis(2000)).await;
+    println!("two second sleep is done");
+}
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
-    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
-    let db = new_sharded_db(5);
-    loop {
-        let (socket, _) = listener.accept().await.unwrap();
-        let db = db.clone();
-        print!("Accepted");
-        tokio::spawn(async move { process(socket, db).await });
+    let _listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+    let mut tasks = vec![];
+    // let db = new_sharded_db(5);
+    for _ in 0..10 {
+        let handle = tokio::spawn(async move {
+            one_second().await;
+        });
+        tasks.push(handle);
     }
+    for _ in 0..10 {
+        let handle = tokio::spawn(async move {
+            two_second().await;
+        });
+
+        tasks.push(handle);
+    }
+    for task in tasks {
+        task.await;
+    }
+    // loop {
+    //     let (socket, _) = listener.accept().await.unwrap();
+    //     let db = db.clone();
+    //     print!("Accepted");
+    //     tokio::spawn(async move { process(socket, db).await });
+    // }
 }
 async fn process(socket: TcpStream, db: shardedDb) {
     use mini_redis::Command::{self, Get, Set};
