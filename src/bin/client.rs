6183aab60fc87039d6c389c033e133a5728abec5
g610g <gio.gonzales@carsu.edu.ph>
2024-05-23 11:34:39 +0800

    client concurrency using mpsc channel

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -10,14 +10,36 @@ enum Command {
 #[tokio::main]
 async fn main() {
     let (tx, mut rx) = mpsc::channel(32);
-    let mut client = client::connect("127.0.0.1:6379").await.unwrap();
+    let tx2 = tx.clone();
 
-    let t1 = tokio::spawn(async {
-        let res = client.get("foo").await;
+    let t1 = tokio::spawn(async move {
+        let cmd = Command::Get {
+            key: "foo".to_string(),
+        };
+        tx.send(cmd).await.unwrap();
     });
-    let t2 = tokio::spawn(async {
-        let res = client.set("foo", "bar".into()).await;
+    let t2 = tokio::spawn(async move {
+        let cmd = Command::Set {
+            key: "foo".to_string(),
+            val: "bar".into(),
+        };
+        tx2.send(cmd).await.unwrap();
+    });
+    let manager = tokio::spawn(async move {
+        let mut client = client::connect("127.0.0.1:6379").await.unwrap();
+        while let Some(cmd) = rx.recv().await {
+            use Command::*;
+            match cmd {
+                Get { key } => {
+                    client.get(&key).await;
+                }
+                Set { key, val } => {
+                    client.set(&key, val).await;
+                }
+            }
+        }
     });
     t1.await.unwrap();
-    t1.await.unwrap();
+    t2.await.unwrap();
+    manager.await.unwrap();
 }
