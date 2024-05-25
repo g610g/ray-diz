686a4c6cb5ecf08243be4b3c3b16cd2e21f13e73
g610g <gio.gonzales@carsu.edu.ph>
2024-05-25 23:38:04 +0800

    with tokio broadcast channel for subscribing

--- src/bin/chat-server.rs
+++ src/bin/chat-server.rs
@@ -1,11 +1,12 @@
 use tokio::{
     io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
     net::{TcpListener, TcpStream},
+    sync::broadcast::{self, Receiver, Sender},
 };
 
 extern crate redis_client;
 
-async fn read_write_buffer(mut socket: TcpStream) {
+async fn read_write_buffer(mut socket: TcpStream, tx: Sender<String>, mut rx: Receiver<String>) {
     let (read, mut write) = socket.split();
     let mut reader = BufReader::new(read);
     let mut line = String::new();
@@ -14,7 +15,10 @@ async fn read_write_buffer(mut socket: TcpStream) {
         if bytes_read == 0 {
             break;
         }
-        write.write_all(&line.as_bytes()).await.unwrap();
+        tx.send(line.clone()).unwrap();
+        let msg = rx.recv().await.unwrap();
+        let _ = write.write_all(&msg.as_bytes()).await.unwrap();
+
         line.clear();
     }
 }
@@ -22,12 +26,16 @@ async fn read_write_buffer(mut socket: TcpStream) {
 async fn main() {
     //binds connection
     let listener = TcpListener::bind("localhost:8000").await.unwrap();
+    let (tx, rx) = broadcast::channel::<String>(10);
     // let mut handles = vec![];
+    //let tx_clone = tx.clone();
     //infinitely accepts client and spawn a task for read and writing buffer
     loop {
+        let tx_clone = tx.clone();
+        let rx_handle = tx.subscribe();
         let (socket, _) = listener.accept().await.unwrap();
         tokio::spawn(async move {
-            read_write_buffer(socket).await;
+            read_write_buffer(socket, tx_clone, rx_handle).await;
         });
     }
 }
